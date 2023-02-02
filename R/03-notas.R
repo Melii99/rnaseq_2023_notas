d0f1255b191b0dd20058e4764e41b060611faa18
Melissa <yuukiakiamano@gmail.com>
2023-02-02 10:07:18 -0600

    Ejemplo 2

--- R/03-notas.R
+++ R/03-notas.R
@@ -29,3 +29,27 @@ vd <- ExploreModelMatrix::VisualizeDesign(
 ## Veamos las imágenes con cowplot
 cowplot::plot_grid(plotlist = vd$plotlist)
 
+## Usaremos shiny otra ves
+app <- ExploreModelMatrix(
+  sampleData = sampleData,
+  designFormula = ~ genotype + treatment
+)
+if (interactive()) shiny::runApp(app)
+
+
+### Ejemplo 2 ###
+
+(sampleData <- data.frame(
+  Response = rep(c("Resistant", "Sensitive"), c(12, 18)),
+  Patient = factor(rep(c(1:6, 8, 11:18), each = 2)),
+  Treatment = factor(rep(c("pre","post"), 15)),
+  ind.n = factor(rep(c(1:6, 2, 5:12), each = 2))))
+
+vd <- VisualizeDesign(
+  sampleData = sampleData,
+  designFormula = ~ Response + Response:ind.n + Response:Treatment,
+  textSizeFitted = 3
+)
+
+cowplot::plot_grid(plotlist = vd$plotlist, ncol = 1)
+
