fd50659a0a158c4e394276066685a75fb26e1c51
Melissa <yuukiakiamano@gmail.com>
2023-02-02 14:15:17 -0600

    Visualizando genes DE

--- R/03-notas.R
+++ R/03-notas.R
@@ -180,3 +180,88 @@ ggplot(as.data.frame(colData(rse_gene_SRP045638)), aes(y = assigned_gene_prop, x
   ylab("Assigned Gene Prop") +
   xlab("Age Group")
 
+### Definimos nuestro modeo estadístico ###
+
+mod <- model.matrix(~ prenatal + sra_attribute.RIN + sra_attribute.sex + assigned_gene_prop,
+                    data = colData(rse_gene_SRP045638)
+
+)
+colnames(mod)
+
+### limma para realizar el análisis de expresión diferencial ###
+
+library("limma")
+vGene <- voom(dge, mod, plot = TRUE)
+
+## coef: numero de columna
+
+eb_results <- eBayes(lmFit(vGene))
+
+de_results <- topTable(
+  eb_results,
+  coef = 2,
+  number = nrow(rse_gene_SRP045638),
+  sort.by = "none"
+)
+dim(de_results)
+
+head(de_results)
+
+## Genes diferencialmente expresados entre pre y post natal con FDR < 5%
+table(de_results$adj.P.Val < 0.05)
+
+## Visualicemos los resultados estadísticos
+plotMA(eb_results, coef = 2)
+
+
+volcanoplot(eb_results, coef = 2, highlight = 3, names = de_results$gene_name)
+
+de_results[de_results$gene_name %in% c("ZSCAN2", "VASH2", "KIAA0922"), ]
+
+
+### Visualizando genes DE ###
+
+## De vGene$E podemos extraer los datos normalizados por limma-voom
+## Extraer valores de los genes de interés
+exprs_heatmap <- vGene$E[rank(de_results$adj.P.Val) <= 50, ]
+
+## Creemos una tabla con información de las muestras
+## y con nombres de columnas más amigables
+df <- as.data.frame(colData(rse_gene_SRP045638)[, c("prenatal", "sra_attribute.RIN", "sra_attribute.sex")])
+colnames(df) <- c("AgeGroup", "RIN", "Sex")
+
+## Hagamos un heatmap
+library("pheatmap")
+pheatmap(
+  exprs_heatmap,
+  cluster_rows = TRUE,
+  cluster_cols = TRUE,
+  show_rownames = FALSE,
+  show_colnames = FALSE,
+  annotation_col = df
+)
+
+### MDS (multidimensional scaling) ###
+
+## Para colores
+library("RColorBrewer")
+
+## Conviertiendo los grupos de edad a colores
+col.group <- df$AgeGroup
+
+levels(col.group) <- brewer.pal(nlevels(col.group), "Set1")
+
+col.group <- as.character(col.group)
+
+## MDS por grupos de edad
+plotMDS(vGene$E, labels = df$AgeGroup, col = col.group)
+
+## Conviertiendo los valores de Sex a colores
+col.sex <- df$Sex
+levels(col.sex) <- brewer.pal(nlevels(col.sex), "Dark2")
+
+col.sex <- as.character(col.sex)
+
+## MDS por sexo
+plotMDS(vGene$E, labels = df$Sex, col = col.sex)
+
