30704e30be3288888a50a9cbafe8e565c4db37dd
Melissa <yuukiakiamano@gmail.com>
2023-02-03 14:12:46 -0600

    Ejercicio speaqeasy_data

---
+++ R/04-notas.R
@@ -0,0 +1,37 @@
+### Ejercicio ###
+
+## Descargamos datos
+speaqeasy_data <- file.path(tempdir(), "rse_speaqeasy.RData")
+download.file("https://github.com/LieberInstitute/SPEAQeasy-example/blob/master/rse_speaqeasy.RData?raw=true", speaqeasy_data, mode = "wb")
+
+## Cargamos SummarizedExperiment
+library("SummarizedExperiment")
+# Cargando objeto rse_gene
+load(speaqeasy_data, verbose = TRUE)
+
+## Exploramos el objeto
+rse_gene
+
+
+### ¿Hay diferencias en totalAssignedGene o mitoRate entre los grupos de diagnosis (PrimaryDx)? ###
+
+## Exploramos PrimaryDx
+table(rse_gene$PrimaryDx)
+
+## Exploramos las diferencias entre grupos de diagnosis para varias variables
+
+# totalAssignedGene
+with(colData(rse_gene), tapply(totalAssignedGene, PrimaryDx, summary))
+
+# mitoRate
+with(colData(rse_gene), tapply(mitoRate, PrimaryDx, summary))
+
+
+### Grafica la expresión de SNAP25 para cada grupo de diagnosis ###
+
+# Explora el objeto rse de forma interactiva
+library("iSEE")
+iSEE::iSEE(rse_gene)
+
+### Sugerir un modelo estadístico para usar en un análisis de expresión diferencial ###
+
